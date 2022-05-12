606df2cd8a779221a779c3a12564fa0321169593
Yiran Hernández <91649913+YahqUQ@users.noreply.github.com>
2022-05-11 23:25:31 -0500

    Clases Para TipoArray
    Se agregan las clases de TipoArray y TipoArrayB

--- src/co/edu/uniquindio/compiladores/modelo/sintaxis/TipoArrayB.kt
+++ src/co/edu/uniquindio/compiladores/modelo/sintaxis/TipoArrayB.kt
@@ -2,8 +2,5 @@ package co.edu.uniquindio.compiladores.modelo.sintaxis
 
 import co.edu.uniquindio.compiladores.modelo.lexico.Token
 
-class TipoArray(tokenActual: Token) {
-
-
-
+class TipoArrayB(tokenActual: Token) {
 }
